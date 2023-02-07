b131e8794eac95364fa660e511612bd52949bdeb
prospero <xyz>
2023-02-07 18:11:52 +0100

    #4 Add Routing

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -9,8 +9,8 @@
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="/" class="<?= urlIs("/") ? "bg-gray-900 text-white" : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about.php" class="<?= urlIs("/about.php") ? "bg-gray-900 text-white" : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                        <a href="/notes.php" class="<?= urlIs("/notes.php") ? "bg-gray-900 text-white" : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Notes</a>
+                        <a href="/about" class="<?= urlIs("/about") ? "bg-gray-900 text-white" : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                        <a href="/notes" class="<?= urlIs("/notes") ? "bg-gray-900 text-white" : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Notes</a>
                     </div>
                 </div>
             </div>
