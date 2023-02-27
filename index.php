df6fc167f499a9642268f7cf27a0d357c49bb6b5
prospero <xyz>
2023-02-27 19:30:37 +0100

    #5 PDO

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
 require("functions.php");
-require("router.php");
+// require("router.php");
+
+$dsn = "mysql:host=localhost;port=3306;dbname=php;user=prospero;password=prospero;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
